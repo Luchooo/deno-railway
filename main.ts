4c524d9861ac9ccab1b61b567b58828766790a3b
Luis Sanchez <luis.udec.sanchez@hotmail.com>
2024-01-28 22:16:05 -0500

    feat: "Create server express"

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-Deno.serve((_request: Request) => {
-  return new Response("Hello, world!");
+import express from "npm:express@4.18.2";
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Welcome to the Dinosaur API!");
 });
+
+const PORT = Deno.env.get("PORT") || 3000;
+app.listen(PORT);
+
+console.log(`API server running on port ${PORT}`);
