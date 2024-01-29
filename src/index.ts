2ea53637288c140b8e97a29851228ced87311e2a
Luis Sanchez <luis.udec.sanchez@hotmail.com>
2024-01-28 22:38:37 -0500

    feat: "Set src folder."

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const port = parseInt(Deno.env.get("PORT") ?? "8080");
+
+const handler = (request: Request): Response => {
+    const body = `Your user-agent is:\n\n${request.headers.get("user-agent") ?? "Unknown"
+        }`;
+
+    return new Response(body, { status: 200 });
+};
+
+console.log(`HTTP server running. Access it at: http://localhost:8080/`);
+Deno.serve({ port }, handler);
